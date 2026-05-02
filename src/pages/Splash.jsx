f92323c989ddfcd18c2aea6c6ae61ba402bbb893
chominseo <singer100@yu.ac.kr>
2026-05-02 18:42:49 +0900

    feat : 2초뒤 로그인 페이지로 이동

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Splash = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/login')
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [navigate])
+
   return (
     // 확인용 페이지
     <div>Splash</div>
