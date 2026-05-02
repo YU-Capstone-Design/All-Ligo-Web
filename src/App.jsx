4c4f55900af278ea45f5520346cb62bbedcfaeb3
chominseo <singer100@yu.ac.kr>
2026-05-02 18:42:30 +0900

    feat : 로그인 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import GlobalLayout from './layout/GlobalLayout'
 import Splash from './pages/Splash'
+import Login from './pages/Login'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route element={<GlobalLayout />}>
           <Route path="/" element={<Splash />} />
+          <Route path="/login" element={<Login/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
