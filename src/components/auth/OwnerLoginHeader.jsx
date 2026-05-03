43ee3f8d1789a708595847bc742e04530f71f1b5
chominseo <singer100@yu.ac.kr>
2026-05-03 18:04:42 +0900

    feat : 소상공인 헤더

---
+++ src/components/auth/OwnerLoginHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import arrowup from '../../assets/arrow-up.svg'
+
+const OwnerLoginHeader = () => {
+  return (
+    <div>
+        <img src={arrowup}/>
+    </div>
+  )
+}
+
+export default OwnerLoginHeader
