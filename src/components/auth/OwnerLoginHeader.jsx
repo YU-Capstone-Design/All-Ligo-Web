e0e0c861792ea309d3dc47ed81f301eb9aee1001
chominseo <singer100@yu.ac.kr>
2026-05-03 18:18:12 +0900

    feat  : 소상공인 Header 컴포넌트

--- src/components/auth/OwnerLoginHeader.jsx
+++ src/components/auth/OwnerLoginHeader.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import arrowup from '../../assets/arrow-up.svg'
 
 const OwnerLoginHeader = () => {
+  const navigate = useNavigate()
+
   return (
-    <div>
-        <img src={arrowup}/>
+    <div className="flex flex-row items-center py-[11px]">
+      <button
+        type="button"
+        onClick={() => navigate(-1)}
+        className="pl-[16px]"
+      >
+        <img src={arrowup} alt="뒤로가기" />
+      </button>
+
+      <div className="text-[14px] w-[314px] leading-[20px] text-center flex justify-center">
+        소상공인 로그인
+      </div>
     </div>
   )
 }
