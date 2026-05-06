c95f28c685b14ee6cb558a3c8cf196b0d7d031ce
chominseo <singer100@yu.ac.kr>
2026-05-06 17:20:59 +0900

    feat : 홍보물 페이지 라우팅 추가

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import OwnerStoreLocation from "./pages/OwnerMembership/OwnerStoreLocation";
 import Thumbnail from "./pages/Thumbnail";
 import OwnerPassword from "./pages/OwnerMembership/OwnerPassword";
 import SuccessPage from "./pages/OwnerMembership/SuccessPage";
+import MakePage from "./pages/MakePage";
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
           <Route path="/login" element={<OwnerLogin />} />
           <Route path="/owner-password" element={<OwnerPassword />} />
           <Route path="/success-page" element={<SuccessPage />} />
+          <Route path='/makepage' element={<MakePage/>} />
         </Route>
       </Routes>
     </BrowserRouter>
