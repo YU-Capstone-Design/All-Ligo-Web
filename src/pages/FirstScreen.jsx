c7981d6ed48f8458022ca10f91b0be1580af477c
chominseo <singer100@yu.ac.kr>
2026-05-17 16:56:00 +0900

    feat : 첫번째 screen 에서 1초뒤 /thumbnail 페이지로 이동

--- src/pages/FirstScreen.jsx
+++ src/pages/FirstScreen.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import ligoicon from '../assets/ligoicon.svg'
 import ligoname from '../assets/ligoname.svg'
+
 const FirstScreen = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/thumbnail')
+    }, 1000)
+
+    return () => clearTimeout(timer)
+  }, [navigate])
+
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-[24.92px]">
-        <img src={ligoicon}/>
-        <img src={ligoname}/>
+      <img src={ligoicon} />
+      <img src={ligoname} />
     </div>
   )
 }
