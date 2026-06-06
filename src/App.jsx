5497de8c94c792f9f961b04033ad24f5af8f9bf4
chominseo <singer100@yu.ac.kr>
2026-06-06 19:47:25 +0900

    feat : 인증실패 UI 구현

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import Notifications from "./pages/Notifications";
 import Guest from "./pages/Guest";
 import GuestCouponList from "./pages/GuestCouponList";
 import AuthSuccess from "./pages/AuthSuccess";
+import AuthFail from "./pages/AuthFail";
 
 import MyPage from "./pages/mypage/Mypage";
 import CouponRegistration from "./pages/mypage/CouponRegistration";
@@ -55,6 +56,7 @@ const App = () => {
           <Route path="/guest" element={<Guest />} />
           <Route path="/guest/coupons" element={<GuestCouponList />} />
           <Route path="/auth-success" element={<AuthSuccess />} />
+          <Route path="/auth-fail" element={<AuthFail />} />
           <Route path="/product/:productId/edit" element={<ProductEdit />} />
           <Route path="/clear/:productId" element={<ClearPage />} />
           <Route path="/makecomplete" element={<MakeComplete />} />
