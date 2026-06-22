9f4f5e2de2b4383cc37ab44d1a1bfec31c6b6805
chominseo <singer100@yu.ac.kr>
2026-06-22 15:32:04 +0900

    fix : 로그아웃 후 alert 창 제거

--- src/pages/mypage/Mypage.jsx
+++ src/pages/mypage/Mypage.jsx
@@ -94,7 +94,7 @@ const MyPage = () => {
   const handleLogout = () => {
     logout();
     setIsSettingsOpen(false);
-    navigate("/splash");
+    navigate("/splash", { replace: true });
   };
 
   useEffect(() => {
